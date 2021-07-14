83a470f494699911145584ab6f589cdd3682bc59
negative1skunk@gmail.com <silaslowe@gmail.com>
2021-07-14 09:20:08 -0500

    refactored email_body to be in get method of newsfeed class

--- main.py
+++ main.py
@@ -4,30 +4,37 @@ from pprint import pprint
 
 
 class NewsFeed:
+    """Representing news articles form newsapi.org based on users interests"""
 
-    def __init__(self, data):
-        self.data = data
+    base_url = 'https://newsapi.org/v2/everything?'
+    api_key = '0f56286be30b49f385e348b38d2c9fe3'
+
+    def __init__(self, interest, from_date, to_date, language):
+        self.interest = interest
+        self.from_date = from_date
+        self.to_date = to_date
+        self.language = language
 
     def get(self):
-        pass
+        url = f'{self.base_url}' \
+              f'q={self.interest}&' \
+              f'from={self.from_date}' \
+              f'to={self.to_date}' \
+              f'&Language={self.language}&' \
+              f'apiKey={self.api_key}'
+
+        response = requests.get(url)
+        content = response.json()
+        articles = content['articles']
+
+        email_body = ''
+
+        for article in articles:
+            email_body = f"{email_body} \n {article['title']}  \n {article['url']} \n \n"
+
+        return email_body
 
 
-url = 'https://newsapi.org/v2/everything?' \
-      'q=nasa&' \
-      'from=2021-07-12&' \
-      'to=2021-07-14' \
-      '&Language=en&' \
-      'apiKey=0f56286be30b49f385e348b38d2c9fe3'
+newsfeed = NewsFeed('nasa', '2021-07-13', '2021-07-14', 'en')
 
-response = requests.get(url)
-content = response.json()
-articles = content['articles']
-
-forEmails = {}
-
-email_body = ''
-
-for article in articles:
-    email_body = f"{email_body} \n {article['title']}  \n {article['url']} \n \n"
-
-print(email_body)
+print(newsfeed.get())
