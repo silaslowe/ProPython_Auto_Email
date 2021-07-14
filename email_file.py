5cc8c869b1ff27b774680962e9985a39f4fafe1b
negative1skunk@gmail.com <silaslowe@gmail.com>
2021-07-14 10:56:44 -0500

    email sends to hard coded email

---
+++ email_file.py
@@ -0,0 +1,7 @@
+import yagmail
+
+email = yagmail.SMTP(user="silaslowe1@gmail.com", password="This!sat3st")
+email.send(to="silaslowe@gmail.com",
+           subject="Hi there",
+           contents="This is Silas.\n\n-Silas",
+           attachments="design.txt")
