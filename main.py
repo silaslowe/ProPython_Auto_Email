c3ebc8fbeb4c30dffe9b6f0333190b2b59ad8097
negative1skunk@gmail.com <silaslowe@gmail.com>
2021-07-14 09:02:18 -0500

    loop for getting titles and urls for articles

--- main.py
+++ main.py
@@ -13,12 +13,21 @@ class NewsFeed:
 
 
 url = 'https://newsapi.org/v2/everything?' \
-      'q=thailand&' \
+      'q=nasa&' \
       'from=2021-07-12&' \
       'to=2021-07-14' \
       '&Language=en&' \
       'apiKey=0f56286be30b49f385e348b38d2c9fe3'
 
 response = requests.get(url)
-content = response.text
-pprint(content)
+content = response.json()
+articles = content['articles']
+
+forEmails = {}
+
+email_body = ''
+
+for article in articles:
+    email_body = f"{email_body} \n {article['title']}  \n {article['url']} \n \n"
+
+print(email_body)
